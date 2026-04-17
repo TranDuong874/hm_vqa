7c2eb0107aa2db22314e6ffa354aa567ee0f431f
Tran Thai Duong <tranthaiduong.dtt@gmail.com>
2026-04-17 12:27:20 +0700

    refactor

--- src/pipeline/__init__.py
+++ src/pipeline/__init__.py
@@ -1,10 +1,8 @@
-from .config import PIPELINE_CONFIG
 from .policies import get_policy, list_policies
 from .tooling import ToolingPlan, build_tooling_plan
 from . import tools
 
 __all__ = [
-    "PIPELINE_CONFIG",
     "ToolingPlan",
     "build_tooling_plan",
     "get_policy",
